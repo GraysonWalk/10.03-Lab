a94529934d02f740526c510bf8eb4dc5777d94b8
GraysonWalk <graysonwalker172@u.boisestate.edu>
2025-10-31 23:28:17 -0600

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,17 @@
 function myLineUp(event) {
     let status = this.status.value;
     let date = this.gameDate.value;
-    let players = this.player.value;
+    let players = [];
+    
+    let checkboxes = this.querySelectorAll('input[name="player"]');
+    for (let i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+          players.push(checkboxes[i].value);
+        }
+    }
 
     console.log(`Status: ${status}, Date: ${date}, Players: ${players.join(", ")}`);
+    
     let output = document.createElement("p");
     output.textContent = `Status: ${status}, Date: ${date}, Players: ${players.join(", ")}`;
     document.body.appendChild(output);
